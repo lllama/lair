8c1737980d26c2383270242d62a77b194a37ab82
Tom Steele <thomasjsteele@gmail.com>
2015-04-08 17:45:48 -0700

    use helper syntax

--- lair/client/views/settings/settings.js
+++ lair/client/views/settings/settings.js
@@ -1,24 +1,26 @@
-Template.settings.allowClientSideUpdates = function () {
-    var setting = Settings.findOne({
-        "setting": "allowClientSideUpdates"
-    });
-    if (typeof setting === 'undefined') {
-        return false;
-    } else {
-        return setting.enabled;
-    }
-};
+Template.settings.helpers({
+    allowClientSideUpdates: function () {
+        var setting = Settings.findOne({
+            setting: 'allowClientSideUpdates'
+        });
+        if (typeof setting === 'undefined') {
+            return false;
+        } else {
+            return setting.enabled;
+        }
+    },
 
-Template.settings.persistViewFilters = function () {
-    var setting = Settings.findOne({
-        "setting": "persistViewFilters"
-    });
-    if (typeof setting === 'undefined') {
-        return false;
-    } else {
-        return setting.enabled;
+    persistViewFilters: function () {
+        var setting = Settings.findOne({
+            setting: 'persistViewFilters'
+        });
+        if (typeof setting === 'undefined') {
+            return false;
+        } else {
+            return setting.enabled;
+        }
     }
-};
+});
 
 Template.settings.events({
     'click .toggle-client-side-updates': function () {
